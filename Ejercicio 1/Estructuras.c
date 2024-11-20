3bc260953dbb39212913bfd76cc74e532e645a88
Gabriel28112005 <gpenatam@myuax.com>
2024-11-20 01:07:50 +0100

    Commit 2

--- Ejercicio 1/Estructuras.c
+++ Ejercicio 1/Estructuras.c
@@ -1,6 +1,6 @@
-//
-// Created by gapt2 on 19/11/2024.
-//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void ejercicio1(){
 
@@ -12,6 +12,22 @@ void ejercicio1(){
     };
 
 // 1.b) Instanciación de estructuras
+
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre,"Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.75;
+
+// 1.c) Instanciación con el operador malloc
+
+    struct Estudiante *estudiante2 = (struct Estudiante*)malloc(sizeof(struct Estudiante));
+    strcpy(estudiante2->nombre,"Maria");
+    estudiante2->edad = 22;
+    estudiante2->promedio = 9.5;
+
+// 1.d) Punteros y estructuras
+
+
 
 
 
